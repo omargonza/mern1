b138a3d75513a8b81d55a1c678d3e04310215940
omargonza <gonzamartinez1081@gmail.com>
2024-01-24 02:19:58 -0300

    nvmrc

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'client',
+    outDir: 'client/dist/',
   },
   optimizeDeps: {
     include: ['react-toastify'],
