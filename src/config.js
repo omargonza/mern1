4323a3646a57336da0f17deef068ece79cea8244
omargonza <gonzamartinez1081@gmail.com>
2024-01-26 09:53:57 -0300

    cors

--- src/config.js
+++ src/config.js
@@ -12,4 +12,4 @@ export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017
 
 export const TOKEN_SECRET = process.env.TOKEN_SECRET ||  "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://mern001.vercel.app";
