d545fb6ee29bb6db4945cf01aa844b9c126dcb1f
omargonza <gonzamartinez1081@gmail.com>
2024-01-26 11:02:23 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -15,9 +15,23 @@ app.use(
     origin: FRONTEND_URL,
   })
 );
+/*
 // Configuración de CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://mern001.vercel.app');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE');
+
+  // Responder a las solicitudes OPTIONS
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+*/
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE');
 
