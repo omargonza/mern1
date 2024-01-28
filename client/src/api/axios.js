090495823414ed5605103c3ff7188b391ab6b22a
omargonza <gonzamartinez1081@gmail.com>
2024-01-27 22:26:09 -0300

    cors

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,21 @@
-import axios from "axios";
-import { API_URL } from "../config.js";
-
 const instance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
+});
+
+// Log de las solicitudes
+instance.interceptors.request.use((config) => {
+  console.log("Solicitud enviada:", config);
+  return config;
+});
+
+// Log de las respuestas
+instance.interceptors.response.use((response) => {
+  console.log("Respuesta recibida:", response);
+  return response;
+}, (error) => {
+  console.error("Error de respuesta:", error);
+  return Promise.reject(error);
 });
 
 export default instance;
