2f17628e64fe5bfc94d9a8d1d9ebb4eab975343b
omargonza <gonzamartinez1081@gmail.com>
2024-01-28 09:27:34 -0300

    map

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -35,122 +35,127 @@ export function LoginPage() {
 
   return (
     <div
+    style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "calc(100vh - 100px)",
+      backgroundColor: "#00000",
+    }}
+  >
+    <Card
       style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "calc(100vh - 100px)",
-        backgroundColor: "#00000",
+        backgroundColor: "#2b2c2e",
+        maxWidth: "400px",
+        width: "100%",
+        padding: "20px",
+        borderRadius: "8px",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        boxSizing: "border-box",
+        margin: "0 auto",
+        overflow: "hidden",
       }}
     >
-      <Card
+      {loginErrors.length > 0 && (
+        <div>
+          {loginErrors.map((error, i) => (
+            <p style={{ color: "red" }} key={i}>
+              {error}
+            </p>
+          ))}
+        </div>
+      )}
+  
+      <h1
         style={{
-          backgroundColor: "#2b2c2e",
-          maxWidth: "400px",
-          width: "100%",
-          padding: "20px",
-          borderRadius: "8px",
-          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-          boxSizing: "border-box",
-          margin: "0 auto",
-          overflow: "hidden",
+          fontSize: "20px",
+          fontWeight: "bold",
+          color: "#585857",
+          marginBottom: "16px",
         }}
       >
-        {loginErrors.map((error, i) => (
-          <p style={{ color: "red" }} key={i}>
-            {error}
-          </p>
-        ))}
-
-        <h1
+        Login
+      </h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        style={{
+          color: "darkgrey",
+          padding: "24px",
+          borderRadius: "8px",
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+          width: "100%",
+        }}
+      >
+        <Label htmlFor="email" style={{ fontWeight: "600", color: "#585857" }}>
+          Email:
+        </Label>
+        <Input
+          name="email"
+          type="text"
+          placeholder="youremail@domain.tld"
+          {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
           style={{
-            fontSize: "20px",
-            fontWeight: "bold",
-            color: "#585857",
-            marginBottom: "16px",
+            width: "100%",
+            padding: "8px",
+            border: "1px solid #ccc",
+            borderRadius: "4px",
+            marginBottom: "8px",
+          }}
+        />
+        <p style={{ color: "red" }}>{errors.email?.message}</p>
+  
+        <Label
+          htmlFor="password"
+          style={{ fontWeight: "600", color: "#585857" }}
+        >
+          Password:
+        </Label>
+        <Input
+          type="password"
+          name="password"
+          placeholder="Write your password"
+          {...register("password", { required: true, minLength: 6 })}
+          style={{
+            width: "100%",
+            padding: "8px",
+            border: "1px solid #ccc",
+            borderRadius: "4px",
+            marginBottom: "8px",
+          }}
+        />
+        <p style={{ color: "red" }}>{errors.password?.message}</p>
+  
+        <Button
+          style={{
+            width: "100%",
+            padding: "10px",
+            backgroundColor: "#007BFF",
+            color: "#fff",
+            borderRadius: "4px",
+            border: "none",
+            cursor: "pointer",
           }}
         >
           Login
-        </h1>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          style={{
-            color: "darkgrey",
-            padding: "24px",
-            borderRadius: "8px",
-            boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-            width: "100%",
-          }}
-        >
-          <Label htmlFor="email" style={{ fontWeight: "600", color: "#585857" }}>
-            Email:
-          </Label>
-          <Input
-            name="email"
-            type="text"
-            placeholder="youremail@domain.tld"
-            {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-            style={{
-              width: "100%",
-              padding: "8px",
-              border: "1px solid #ccc",
-              borderRadius: "4px",
-              marginBottom: "8px",
-            }}
-          />
-          <p style={{ color: "red" }}>{errors.email?.message}</p>
-
-          <Label
-            htmlFor="password"
-            style={{ fontWeight: "600", color: "#585857" }}
-          >
-            Password:
-          </Label>
-          <Input
-            type="password"
-            name="password"
-            placeholder="Write your password"
-            {...register("password", { required: true, minLength: 6 })}
-            style={{
-              width: "100%",
-              padding: "8px",
-              border: "1px solid #ccc",
-              borderRadius: "4px",
-              marginBottom: "8px",
-            }}
-          />
-          <p style={{ color: "red" }}>{errors.password?.message}</p>
-
-          <Button
-            style={{
-              width: "100%",
-              padding: "10px",
-              backgroundColor: "#007BFF",
-              color: "#fff",
-              borderRadius: "4px",
-              border: "none",
-              cursor: "pointer",
-            }}
-          >
-            Login
-          </Button>
-        </form>
-
-        <p
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            fontSize: "14px",
-            color: "#585857",
-          }}
-        >
-          ¿No tienes una cuenta?{" "}
-          <Link to="/register" style={{ color: "#585857" }}>
-            Sign up
-          </Link>
-        </p>
-      </Card>
-      </div>
+        </Button>
+      </form>
+  
+      <p
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          fontSize: "14px",
+          color: "#585857",
+        }}
+      >
+        ¿No tienes una cuenta?{" "}
+        <Link to="/register" style={{ color: "#585857" }}>
+          Sign up
+        </Link>
+      </p>
+    </Card>
+  </div>
+  
   
   );
 }
